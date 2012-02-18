098b2b8b39418da2ae3078aa6715a76b245e3153
Javier Marin <javiermarinros@gmail.com>
2012-02-18 13:16:56 +0000

    Updated codemirror

--- dump-static/dump.js
+++ dump-static/dump.js
@@ -11,7 +11,7 @@ if (!dump_initialized) {
         load_overlay();
 
         //Añadir visor HTML dinámico para las cadenas de texto
-        load_html_viewer();
+        load_string_viewer();
 
         //Añadir visor de código
         load_source_code_viewer();
@@ -24,7 +24,7 @@ function load_toggles() {
     $.fn.single_double_click = function (single_click_callback, double_click_callback, timeout) {
         return this.each(function () {
             var clicks = 0,
-                self = this;
+            self = this;
             if ($.browser.msie) { // ie triggers dblclick instead of click if they are fast
                 $(this).bind("dblclick", function (event) {
                     clicks = 2;
@@ -98,8 +98,7 @@ function load_overlay() {
 
     //Establecer delegado que procese la superposición de los elementos
     $('.dump-expand').click(function () {
-        var dump = $(this).parents('.dump-main').first();
-        var overlay_button = $(dump).find('.dump-expand');
+        var dump = $(this).closest('.dump-main');
 
         //Crear capa de superposición a todo el body (tipo 'fancybox')
         var overlay = $('<div id="dump-overlay"></div>').hide().appendTo("body");
@@ -123,7 +122,6 @@ function load_overlay() {
                     $(overlay).remove();
                 });
                 $(close_button).remove(); //Borrar botón de cerrar
-                $(overlay_button).show(); //Volver a mostrar el botón para superponer
                 $(dummy).replaceWith(dump); //Sustituir señuelo por el original
                 $(dump).removeClass('overlay').css('top', '').show(); //Volver a colocar el elemento en su sitio
                 toggle_element_content(dump, false, '', true); //Contraer nodo           
@@ -132,29 +130,30 @@ function load_overlay() {
 
         //Superponer volcado de datos
         $(dump).addClass('overlay').css('top', $(window).scrollTop() + 125).appendTo('body');
-        $(overlay_button).hide(); //Ocultar el botón para superponer
         toggle_element_content(dump, true, '', false); //Expandir nodo
+        
         //Mostrar
         $(overlay).fadeTo('fast', 0.7);
         $(dump).fadeTo('fast', 1);
     });
 }
 
-function load_html_viewer() {
+function load_string_viewer() {
     $('.dump-string').each(function () {
-        $(this).html('<div class="dump-string-tools"><span class="dump-string-plain dump-selected">PLAIN</span> <span class="dump-string-html">HTML</span></div><div class="dump-string-text">' + $(this).html() + '</div>');
+        $(this).html('<div class="dump-string-tools"><span class="dump-string-format-plain dump-selected">PLAIN</span> <span class="dump-string-format-html">HTML</span></div><div class="dump-string-text">' + $(this).html() + '</div>');
     });
-    $('.dump-string-plain, .dump-string-html').click(function () {
+    $('.dump-string-format-plain, .dump-string-format-html').click(function () {
         if ($(this).hasClass('dump-selected')) return;
 
-        $(this).parents('.dump-string-tools').children().removeClass('dump-selected');
+        //   $(this).parents('.dump-string-tools').children().removeClass('dump-selected');
+        $(this).siblings().removeClass('dump-selected');
         $(this).addClass('dump-selected');
 
-        var textDiv = $(this).parents('.dump-string').find('.dump-string-text');
-        if ($(this).hasClass('dump-string-plain')) { //Mostrar texto plano
-            $(textDiv).text($(textDiv).html());
+        var textDiv = $(this).closest('.dump-string').find('.dump-string-text, .dump-string-html');
+        if ($(this).hasClass('dump-string-format-plain')) { //Mostrar texto plano
+            $(textDiv).text($(textDiv).html()).attr('class','dump-string-text');
         } else { //Mostrar HTML
-            $(textDiv).html($(textDiv).text());
+            $(textDiv).html($(textDiv).text()).attr('class','dump-string-html');
         }
     });
 }
@@ -166,8 +165,8 @@ function load_source_code_viewer() {
             var i = 1;
             var ii = scripts.length;
             var onScriptLoaded = function (data, response) {
-                    if (i++ == ii) onComplete();
-                };
+                if (i++ == ii) onComplete();
+            };
             for (var s in scripts) {
                 $.getScript(scripts[s], onScriptLoaded);
             };
